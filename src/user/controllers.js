a6b089ba182b196f31451a115272645c9ce19e93
Edward Valentine <eddieval2022@gmail.com>
2022-07-04 22:55:34 +0100

    Delete added and working

--- src/user/controllers.js
+++ src/user/controllers.js
@@ -18,15 +18,7 @@ exports.findUsers = async (req, res) => {
     res.send({ error });
   }
 };
-exports.deleteUser = async (req, res) => {
-  const removeUser = await User.deleteOne(req.body);
-  res.send({ user: removeUser });
-  try {
-  } catch (error) {
-    console.log(error);
-    res.send({ error });
-  }
-};
+
 exports.deleteUser = async (req, res) => {
   const delUser = await User.deleteOne(req.body);
   res.send({ user: delUser });
